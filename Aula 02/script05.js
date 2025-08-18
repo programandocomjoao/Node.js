a12c32323cb4e76ba32e97bf3663c61e40d9ebd6
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-08-18 10:26:13 -0300

    Delete Aula 02 directory

--- Aula 02/script05.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type':'text/plain; charset=UTF-8'});
-
-	res.end('Usuário cadastrado com sucesso!');
-}).listen(8080);
-
-console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
