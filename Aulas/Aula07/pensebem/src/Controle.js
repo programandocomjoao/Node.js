efb19d09bb493c1451e5b30d6be8caf0a2c0a9e7
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-22 21:04:05 -0300

    Add files via upload

--- Aulas/Aula07/pensebem/src/Controle.js
+++ Aulas/Aula07/pensebem/src/Controle.js
@@ -3,6 +3,8 @@ import Dados from "./Dados"
 function Controle() {
   const jogar = (resposta) => {
     if(resposta == Dados.gabarito[Dados.pergunta-1]) {
+      document.getElementById('tela1').style.border = 'green 3px solid'
+
       if(Dados.tentativa == 1)
         Dados.pontos += 3
       else if(Dados.tentativa == 2)
@@ -12,7 +14,6 @@ function Controle() {
 
       Dados.pergunta++
       Dados.tentativa = 1
-      document.getElementById('tela1').style.border = 'black 1px solid'
     }
     else {
       document.getElementById('tela1').style.border = 'red 3px solid'
@@ -21,7 +22,6 @@ function Controle() {
       if(Dados.tentativa > 3) {
         Dados.pergunta++
         Dados.tentativa = 1
-        document.getElementById('tela1').style.border = 'black 1px solid'
       }
     }
 
@@ -29,18 +29,19 @@ function Controle() {
     document.getElementById('tela2').value = `Tentativa ${Dados.tentativa} de 3`
 
     if(Dados.pergunta > 5) {
+      document.getElementById('tela1').style.border = 'black 1px solid'
       document.getElementById('tela1').value = '***FIM***'
-      document.getElementById('tela2').value = `Pontuação: ${Dados.pontos}`
+      document.getElementById('tela2').value = `PONTOS: ${Dados.pontos}`
     }
   }
 
   return(
-    <>
+    <p>
       <input type="button" id="botaoA" value="A" onClick={() => jogar('A')} />
       <input type="button" id="botaoB" value="B" onClick={() => jogar('B')} />
       <input type="button" id="botaoC" value="C" onClick={() => jogar('C')} />
       <input type="button" id="botaoD" value="D" onClick={() => jogar('D')} />
-    </>
+    </p>
   )
 }
 
